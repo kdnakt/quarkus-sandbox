6b871d73ae5b2fa4bff32445be461033495af8ae
kdnakt <a.kid.1985@gmail.com>
2019-12-14 11:30:12 +0900

    fix lambda role

--- quarkus-lambda-dynamodb/create-native.sh
+++ quarkus-lambda-dynamodb/create-native.sh
@@ -1,1 +1,1 @@
-aws lambda create-function --function-name my-native-function --zip-file fileb://target/function.zip --handler any.name.not.used --runtime provided --role arn:aws:iam::1234567:role/lambda-cli-role --environment Variables="{DISABLE_SIGNAL_HANDLERS=true}"
+aws lambda create-function --function-name my-native-function --zip-file fileb://target/function.zip --handler any.name.not.used --runtime provided --role arn:aws:iam::195905244530:role/aws-lambda-basic-role --environment Variables="{DISABLE_SIGNAL_HANDLERS=true}"
--- quarkus-lambda-dynamodb/create.sh
+++ quarkus-lambda-dynamodb/create.sh
@@ -1,1 +1,1 @@
-aws lambda create-function --function-name my-function --zip-file fileb://target/quarkus-lambda-dynamodb-1.0-SNAPSHOT-runner.jar --handler io.quarkus.amazon.lambda.runtime.QuarkusStreamHandler::handleRequest --runtime java8 --role arn:aws:iam::1234567:role/lambda-cli-role
+aws lambda create-function --function-name my-function --zip-file fileb://target/quarkus-lambda-dynamodb-1.0-SNAPSHOT-runner.jar --handler io.quarkus.amazon.lambda.runtime.QuarkusStreamHandler::handleRequest --runtime java8 --role arn:aws:iam::195905244530:role/aws-lambda-basic-role
