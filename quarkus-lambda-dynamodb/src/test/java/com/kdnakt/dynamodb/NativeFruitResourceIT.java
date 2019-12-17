68330bf4ca5f94acd6d54690d7f00b0864c38ec5
kdnakt <a.kid.1985@gmail.com>
2019-12-17 09:25:31 +0900

    remove tests for native compile

--- quarkus-lambda-dynamodb/src/test/java/com/kdnakt/dynamodb/NativeFruitResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.kdnakt.dynamodb;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeFruitResourceIT extends FruitResourceTest {
-
-    // Execute the same tests but in native mode.
-}
