2f123d1fd2dea100fc45b3389aa3f0ad32a74199
kdnakt <a.kid.1985@gmail.com>
2020-01-02 17:12:54 +0900

    fix test

--- rest-json/src/test/java/com/kdnakt/quarkus/rest/json/FruitResourceTest.java
+++ rest-json/src/test/java/com/kdnakt/quarkus/rest/json/FruitResourceTest.java
@@ -4,7 +4,7 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.containsString;
 
 @QuarkusTest
 public class FruitResourceTest {
@@ -15,7 +15,9 @@ public class FruitResourceTest {
           .when().get("/fruits")
           .then()
              .statusCode(200)
-             .body(is("hello"));
+             .body(
+                 containsString("Apple"), containsString("Winter fruit"),
+                 containsString("Pineapple"), containsString("Tropical fruit"));
     }
 
 }
