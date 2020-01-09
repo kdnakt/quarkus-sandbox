583012c6edcf2a459fb1913b2251a51bf247deb9
kdnakt <a.kid.1985@gmail.com>
2020-01-09 09:41:18 +0900

    use application.properties

--- config-quickstart/src/test/java/com/kdnakt/quarkus/config/GreetingResourceTest.java
+++ config-quickstart/src/test/java/com/kdnakt/quarkus/config/GreetingResourceTest.java
@@ -15,7 +15,7 @@ public class GreetingResourceTest {
           .when().get("/greeting")
           .then()
              .statusCode(200)
-             .body(is("hello"));
+             .body(is("hello quarkus!!"));
     }
 
 }
