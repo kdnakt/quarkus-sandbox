13545852f380fb8491f2b6bd857e274c351b49f2
kdnakt <a.kid.1985@gmail.com>
2020-01-10 09:09:19 +0900

    use config class

--- config-quickstart/src/main/java/com/kdnakt/quarkus/config/GreetingResource.java
+++ config-quickstart/src/main/java/com/kdnakt/quarkus/config/GreetingResource.java
@@ -1,28 +1,21 @@
 package com.kdnakt.quarkus.config;
 
-import java.util.Optional;
-
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-
 @Path("/greeting")
 public class GreetingResource {
-    @ConfigProperty(name = "greeting.message")
-    String message;
-
-    @ConfigProperty(name = "greeting.suffix", defaultValue = "!")
-    String suffix;
-
-    @ConfigProperty(name = "greeting.name")
-    Optional<String> name;
+    @Inject
+    GreetingConfig config;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return message + " " + name.orElse("world") + suffix;
+        return config.getMessage() + " "
+                + config.getName().orElse("world")
+                + config.getSuffix();
     }
 }
