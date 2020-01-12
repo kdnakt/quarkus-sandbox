968446c8a9adef2f92fdd19d573caad50851a18a
kdnakt <a.kid.1985@gmail.com>
2020-01-12 22:29:29 +0900

    yaml-config project

---
+++ yaml-config/src/test/java/com/kdnakt/quarkus/yaml/config/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package com.kdnakt.quarkus.yaml.config;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
