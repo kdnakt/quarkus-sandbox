eebf491eae5e605775a4a132dd4ef5ef88de3bb2
kdnakt <a.kid.1985@gmail.com>
2020-01-13 12:49:44 +0900

    use LaunchMode

--- lifecycle-quickstart/src/main/java/com/kdnakt/quarkus/events/AppLifecycleBean.java
+++ lifecycle-quickstart/src/main/java/com/kdnakt/quarkus/events/AppLifecycleBean.java
@@ -1,8 +1,10 @@
-package  com.kdnakt.quarkus.events;
+package com.kdnakt.quarkus.events;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
+import javax.inject.Inject;
 
+import io.quarkus.runtime.LaunchMode;
 import io.quarkus.runtime.ShutdownEvent;
 import io.quarkus.runtime.StartupEvent;
 import org.jboss.logging.Logger;
@@ -12,12 +14,15 @@ public class AppLifecycleBean {
 
     private static final Logger LOGGER = Logger.getLogger("ListenerBean");
 
+    @Inject
+    LaunchMode mode;
+
     void onStart(@Observes StartupEvent ev) {               
-        LOGGER.info("The application is starting...");
+        LOGGER.info("The application is starting..." + mode.name());
     }
 
     void onStop(@Observes ShutdownEvent ev) {               
-        LOGGER.info("The application is stopping...");
+        LOGGER.info("The application is stopping..." + LaunchMode.current());
     }
 
 }
