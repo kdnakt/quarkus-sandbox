a8de4f63e4aa474765f3be2a776357a8a69ea26a
kdnakt <a.kid.1985@gmail.com>
2020-01-15 03:12:18 +0900

    add manual-validation

--- validation-quickstart/src/main/java/com/kdnakt/quarkus/validation/BookResource.java
+++ validation-quickstart/src/main/java/com/kdnakt/quarkus/validation/BookResource.java
@@ -1,8 +1,14 @@
 package com.kdnakt.quarkus.validation;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import javax.inject.Inject;
+import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -17,5 +23,44 @@ public class BookResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "hello";
+    }
+
+    @Path("/manual-validation")
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Result tryMeManualValidation(Book book) {
+        System.out.println("hogehoge");
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        if (violations.isEmpty()) {
+            return new Result("Book is valid! It was validated by manual validation.");
+        } else {
+            return new Result(violations);
+        }
+    }
+
+    private class Result {
+
+        Result(String message) {
+            this.success = true;
+            this.message = message;
+        }
+
+        Result(Set<? extends ConstraintViolation<?>> violations) {
+            this.success = false;
+            this.message = violations.stream().map(cv -> cv.getMessage()).collect(Collectors.joining(", "));
+        }
+
+        private String message;
+        private boolean success;
+
+        public String getMessage() {
+            return message;
+        }
+
+        public boolean isSuccess() {
+            return success;
+        }
+
     }
 }
