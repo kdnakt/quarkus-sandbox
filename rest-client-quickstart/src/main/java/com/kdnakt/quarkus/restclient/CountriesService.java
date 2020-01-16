1fbe05aba15528c8bf342204835527e89c95cd77
kdnakt <a.kid.1985@gmail.com>
2020-01-17 07:42:40 +0900

    add CountriesService

---
+++ rest-client-quickstart/src/main/java/com/kdnakt/quarkus/restclient/CountriesService.java
@@ -0,0 +1,19 @@
+package com.kdnakt.quarkus.restclient;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import java.util.Set;
+
+@Path("/v2")
+@RegisterRestClient
+public interface CountriesService {
+
+    @GET
+    @Path("/name/{name}")
+    @Produces("application/json")
+    Set<Country> getByName(@PathParam String name);
+}
