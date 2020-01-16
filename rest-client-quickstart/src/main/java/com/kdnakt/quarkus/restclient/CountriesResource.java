c79b56e9d921266f64e3847d20dcc7551063e05e
kdnakt <a.kid.1985@gmail.com>
2020-01-17 07:52:24 +0900

    fix CountriesResource

--- rest-client-quickstart/src/main/java/com/kdnakt/quarkus/restclient/CountriesResource.java
+++ rest-client-quickstart/src/main/java/com/kdnakt/quarkus/restclient/CountriesResource.java
@@ -1,16 +1,27 @@
 package com.kdnakt.quarkus.restclient;
 
+import java.util.Set;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
 @Path("/country")
 public class CountriesResource {
 
+    @Inject
+    @RestClient
+    CountriesService countriesService;
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    @Path("/name/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Set<Country> name(@PathParam String name) {
+        return countriesService.getByName(name);
     }
 }
