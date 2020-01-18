0384832f9c6c91c16f10b528ccf6928dbfc416d8
kdnakt <a.kid.1985@gmail.com>
2020-01-18 21:46:50 +0900

    add sendFile resource

--- rest-client-multipart-quickstart/src/main/java/com/kdnakt/quarkus/restclient/multipart/MultipartClientResource.java
+++ rest-client-multipart-quickstart/src/main/java/com/kdnakt/quarkus/restclient/multipart/MultipartClientResource.java
@@ -1,16 +1,29 @@
 package com.kdnakt.quarkus.restclient.multipart;
 
-import javax.ws.rs.GET;
+import java.io.ByteArrayInputStream;
+
+import javax.inject.Inject;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
 @Path("/client")
 public class MultipartClientResource {
 
-    @GET
+    @Inject
+    @RestClient
+    MultipartService service;
+
+    @POST
+    @Path("/multipart")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    public String sendFile() throws Exception {
+        MultipartBody body = new MultipartBody();
+        body.fileName = "greeting.txt";
+        body.file = new ByteArrayInputStream("HELLO WORLD".getBytes());
+        return service.sendMultipartData(body);
     }
 }
