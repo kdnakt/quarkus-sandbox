3e385289b0b1f8e201f6f09941101f1153927af6
kdnakt <a.kid.1985@gmail.com>
2020-01-18 21:51:15 +0900

    mod test

--- rest-client-multipart-quickstart/src/test/java/com/kdnakt/quarkus/restclient/multipart/MultipartClientResourceTest.java
+++ rest-client-multipart-quickstart/src/test/java/com/kdnakt/quarkus/restclient/multipart/MultipartClientResourceTest.java
@@ -4,7 +4,7 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.containsString;
 
 @QuarkusTest
 public class MultipartClientResourceTest {
@@ -12,10 +12,13 @@ public class MultipartClientResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/client")
-          .then()
-             .statusCode(200)
-             .body(is("hello"));
+            .when().post("/client/multipart")
+            .then()
+            .statusCode(200)
+            .body(containsString("Content-Disposition: form-data; name=\"file\""),
+                    containsString("HELLO WORLD"),
+                    containsString("Content-Disposition: form-data; name=\"fileName\""),
+                    containsString("greeting.txt"));
     }
 
 }
