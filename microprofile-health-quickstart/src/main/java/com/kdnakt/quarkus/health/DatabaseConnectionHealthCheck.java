9c4b84d99c5f83ef66f878b9ad91ef412cd03c83
kdnakt <a.kid.1985@gmail.com>
2020-01-18 12:57:03 +0900

    use property to check database health

--- microprofile-health-quickstart/src/main/java/com/kdnakt/quarkus/health/DatabaseConnectionHealthCheck.java
+++ microprofile-health-quickstart/src/main/java/com/kdnakt/quarkus/health/DatabaseConnectionHealthCheck.java
@@ -1,7 +1,9 @@
 package com.kdnakt.quarkus.health;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
 import org.eclipse.microprofile.health.Readiness;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -10,8 +12,28 @@ import javax.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class DatabaseConnectionHealthCheck implements HealthCheck {
 
+    @ConfigProperty(name = "database.up", defaultValue = "false")
+    private boolean databaseUp;
+
     @Override
     public HealthCheckResponse call() {
-        return HealthCheckResponse.up("Database connection health check");
+
+        HealthCheckResponseBuilder responseBuilder = HealthCheckResponse.named("Database connection health check");
+
+        try {
+            simulateDatabaseConnectionVerification();
+            responseBuilder.up();
+        } catch (IllegalStateException e) {
+            // cannot access the database
+            responseBuilder.down();
+        }
+
+        return responseBuilder.build();
+    }
+
+    private void simulateDatabaseConnectionVerification() {
+        if (!databaseUp) {
+            throw new IllegalStateException("Cannot contact database");
+        }
     }
 }
