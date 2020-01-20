b14e0c28e7de6d610cd93f95cb85d102484ed210
kdnakt <a.kid.1985@gmail.com>
2020-01-20 09:50:41 +0900

    use PermitAll

--- security-jwt-quickstart/src/main/java/com/kdnakt/quarkus/jwt/TokenSecuredResource.java
+++ security-jwt-quickstart/src/main/java/com/kdnakt/quarkus/jwt/TokenSecuredResource.java
@@ -1,16 +1,38 @@
 package com.kdnakt.quarkus.jwt;
 
+import java.security.Principal;
+
+import javax.annotation.security.PermitAll;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
 
+import org.eclipse.microprofile.jwt.JsonWebToken;
+
+/**
+ * Version 1 of the TokenSecuredResource
+ */
 @Path("/secured")
+@RequestScoped 
 public class TokenSecuredResource {
 
-    @GET
+    @Inject
+    JsonWebToken jwt; 
+
+    @GET()
+    @Path("permit-all")
+    @PermitAll 
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    public String hello(@Context SecurityContext ctx) { 
+        Principal caller =  ctx.getUserPrincipal(); 
+        String name = caller == null ? "anonymous" : caller.getName();
+        boolean hasJWT = jwt.getClaimNames() != null;
+        String helloReply = String.format("hello + %s, isSecure: %s, authScheme: %s, hasJWT: %s", name, ctx.isSecure(), ctx.getAuthenticationScheme(), hasJWT);
+        return helloReply; 
     }
 }
