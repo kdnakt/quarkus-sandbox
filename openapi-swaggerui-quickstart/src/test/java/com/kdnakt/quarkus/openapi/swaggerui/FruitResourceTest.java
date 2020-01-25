ba802333cdf64807a6fcbcfebdfa01ad731dab43
kdnakt <a.kid.1985@gmail.com>
2020-01-25 11:15:09 +0900

    fix test

--- openapi-swaggerui-quickstart/src/test/java/com/kdnakt/quarkus/openapi/swaggerui/FruitResourceTest.java
+++ openapi-swaggerui-quickstart/src/test/java/com/kdnakt/quarkus/openapi/swaggerui/FruitResourceTest.java
@@ -3,19 +3,49 @@ package com.kdnakt.quarkus.openapi.swaggerui;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
+import javax.ws.rs.core.MediaType;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 
 @QuarkusTest
 public class FruitResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    public void testList() {
         given()
-          .when().get("/fruits")
-          .then()
-             .statusCode(200)
-             .body(is("hello"));
+                .when().get("/fruits")
+                .then()
+                .statusCode(200)
+                .body("$.size()", is(2),
+                        "name", containsInAnyOrder("Apple", "Pineapple"),
+                        "description", containsInAnyOrder("Winter fruit", "Tropical fruit"));
+    }
+
+    @Test
+    public void testAdd() {
+        given()
+                .body("{\"name\": \"Pear\", \"description\": \"Winter fruit\"}")
+                .header("Content-Type", MediaType.APPLICATION_JSON)
+                .when()
+                .post("/fruits")
+                .then()
+                .statusCode(200)
+                .body("$.size()", is(3),
+                        "name", containsInAnyOrder("Apple", "Pineapple", "Pear"),
+                        "description", containsInAnyOrder("Winter fruit", "Tropical fruit", "Winter fruit"));
+
+        given()
+                .body("{\"name\": \"Pear\", \"description\": \"Winter fruit\"}")
+                .header("Content-Type", MediaType.APPLICATION_JSON)
+                .when()
+                .delete("/fruits")
+                .then()
+                .statusCode(200)
+                .body("$.size()", is(2),
+                        "name", containsInAnyOrder("Apple", "Pineapple"),
+                        "description", containsInAnyOrder("Winter fruit", "Tropical fruit"));
     }
 
 }
