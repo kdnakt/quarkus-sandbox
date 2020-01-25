8cfa549d94346bbc83a8c02407b806487a40e767
kdnakt <a.kid.1985@gmail.com>
2020-01-25 11:12:38 +0900

    add fruit pojo

---
+++ openapi-swaggerui-quickstart/src/main/java/com/kdnakt/quarkus/openapi/swaggerui/Fruit.java
@@ -0,0 +1,15 @@
+package com.kdnakt.quarkus.openapi.swaggerui;
+
+public class Fruit {
+
+    public String name;
+    public String description;
+
+    public Fruit() {
+    }
+
+    public Fruit(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
