520098d45f71926d67b57a25f6d574f5c52d1750
kdnakt <a.kid.1985@gmail.com>
2020-01-26 11:17:25 +0900

    add fault-torelance project

---
+++ microprofile-fault-tolerance-quickstart/src/test/java/com/kdnakt/quarkus/faulttolerance/CoffeeResourceTest.java
@@ -0,0 +1,21 @@
+package com.kdnakt.quarkus.faulttolerance;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class CoffeeResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/coffee")
+          .then()
+             .statusCode(200)
+             .body(is("hello"));
+    }
+
+}
---
+++ microprofile-fault-tolerance-quickstart/src/test/java/com/kdnakt/quarkus/faulttolerance/NativeCoffeeResourceIT.java
@@ -0,0 +1,9 @@
+package com.kdnakt.quarkus.faulttolerance;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeCoffeeResourceIT extends CoffeeResourceTest {
+
+    // Execute the same tests but in native mode.
+}
