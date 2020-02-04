fb0fcc0040b6b04d1323b73d43e4fee849b53e20
kdnakt <a.kid.1985@gmail.com>
2020-02-05 08:13:08 +0900

    add spring-di project

---
+++ spring-di-quickstart/src/test/java/com/kdnakt/quarkus/spring/di/NativeGreeterResourceIT.java
@@ -0,0 +1,9 @@
+package com.kdnakt.quarkus.spring.di;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreeterResourceIT extends GreeterResourceTest {
+
+    // Execute the same tests but in native mode.
+}
