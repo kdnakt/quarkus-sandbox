1caa914770157f784bc6b87523b6fc27d6aed58a
kdnakt <a.kid.1985@gmail.com>
2020-02-06 01:35:42 +0900

    fix GreeterResource

--- spring-di-quickstart/src/main/java/com/kdnakt/quarkus/spring/di/GreeterResource.java
+++ spring-di-quickstart/src/main/java/com/kdnakt/quarkus/spring/di/GreeterResource.java
@@ -5,12 +5,17 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 @Path("/greeting")
 public class GreeterResource {
+
+    @Autowired
+    GreeterBean greeterBean;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "hello";
+        return greeterBean.greet("world");
     }
 }
