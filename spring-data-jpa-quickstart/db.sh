958859b38270b5fe2b03fea896f5cc5c49c72d7e
kdnakt <a.kid.1985@gmail.com>
2020-02-11 10:05:34 +0900

    add db.sh

---
+++ spring-data-jpa-quickstart/db.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker run --ulimit memlock=-1:-1 -it --rm=true --memory-swappiness=0 --name quarkus_test -e POSTGRES_USER=quarkus_test -e POSTGRES_PASSWORD=quarkus_test -e POSTGRES_DB=quarkus_test -p 5432:5432 postgres:11.5
