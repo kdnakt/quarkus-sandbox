f97b6e73a45de2be25e66dd352287990df9c2447
kdnakt <a.kid.1985@gmail.com>
2020-02-11 09:57:07 +0900

    add spring-data-jpa project

---
+++ spring-data-jpa-quickstart/src/test/java/com/kdnakt/quarkus/spring/data/jpa/NativeFruitResourceIT.java
@@ -0,0 +1,9 @@
+package com.kdnakt.quarkus.spring.data.jpa;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeFruitResourceIT extends FruitResourceTest {
+
+    // Execute the same tests but in native mode.
+}
