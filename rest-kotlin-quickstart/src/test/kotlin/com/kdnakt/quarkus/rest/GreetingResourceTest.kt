3787ca1e7974ed9a7c10d4f9c45e70b80131bfe1
kdnakt <a.kid.1985@gmail.com>
2020-02-16 09:39:06 +0900

    add kotlin project

---
+++ rest-kotlin-quickstart/src/test/kotlin/com/kdnakt/quarkus/rest/GreetingResourceTest.kt
@@ -0,0 +1,20 @@
+package com.kdnakt.quarkus.rest
+
+import io.quarkus.test.junit.QuarkusTest
+import io.restassured.RestAssured.given
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.jupiter.api.Test
+
+@QuarkusTest
+open class GreetingResourceTest {
+
+    @Test
+    fun testHelloEndpoint() {
+        given()
+          .`when`().get("/greeting")
+          .then()
+             .statusCode(200)
+             .body(`is`("hello"))
+    }
+
+}
---
+++ rest-kotlin-quickstart/src/test/kotlin/com/kdnakt/quarkus/rest/NativeGreetingResourceIT.kt
@@ -0,0 +1,6 @@
+package com.kdnakt.quarkus.rest
+
+import io.quarkus.test.junit.NativeImageTest
+
+@NativeImageTest
+open class NativeGreetingResourceIT : GreetingResourceTest()
