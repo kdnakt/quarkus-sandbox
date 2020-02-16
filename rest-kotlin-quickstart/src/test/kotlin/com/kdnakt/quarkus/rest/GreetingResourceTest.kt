740e1bc6563c42449b79a690d18a032dca50d107
kdnakt <a.kid.1985@gmail.com>
2020-02-16 15:48:54 +0900

    fix test

--- rest-kotlin-quickstart/src/test/kotlin/com/kdnakt/quarkus/rest/GreetingResourceTest.kt
+++ rest-kotlin-quickstart/src/test/kotlin/com/kdnakt/quarkus/rest/GreetingResourceTest.kt
@@ -2,7 +2,7 @@ package com.kdnakt.quarkus.rest
 
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.given
-import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.equalTo
 import org.junit.jupiter.api.Test
 
 @QuarkusTest
@@ -14,7 +14,7 @@ open class GreetingResourceTest {
           .`when`().get("/greeting")
           .then()
              .statusCode(200)
-             .body(`is`("hello"))
+             .body("message", equalTo("hello kotlin!"))
     }
 
 }
