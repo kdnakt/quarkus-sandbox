ac391d8f81d550265b683dfba220ae770261cd76
kdnakt <a.kid.1985@gmail.com>
2020-02-27 00:03:44 +0900

    add scheduler project

---
+++ scheduler-quickstart/src/test/java/com/kdnakt/quarkus/scheduling/NativeCountResourceIT.java
@@ -0,0 +1,9 @@
+package com.kdnakt.quarkus.scheduling;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeCountResourceIT extends CountResourceTest {
+
+    // Execute the same tests but in native mode.
+}
