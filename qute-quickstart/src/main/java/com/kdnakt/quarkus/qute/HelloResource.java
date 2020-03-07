2e5b701090ed462493ffe73ca02077bf61e70cac
kdnakt <a.kid.1985@gmail.com>
2020-03-08 07:52:54 +0900

    use hello template

--- qute-quickstart/src/main/java/com/kdnakt/quarkus/qute/HelloResource.java
+++ qute-quickstart/src/main/java/com/kdnakt/quarkus/qute/HelloResource.java
@@ -1,16 +1,26 @@
 package com.kdnakt.quarkus.qute;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
 
 @Path("/hello")
 public class HelloResource {
 
+    @Inject
+    Template hello;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    public TemplateInstance hello(
+        @QueryParam("name") String name
+    ) {
+        return hello.data("name", name);
     }
 }
