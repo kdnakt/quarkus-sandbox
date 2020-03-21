8109befa17648a3574277fca5a5039fd2b49dbe8
kdnakt <a.kid.1985@gmail.com>
2020-03-21 14:45:59 +0900

    try cache config

--- cache-quickstart/src/main/java/com/kdnakt/quarkus/cache/WeatherForecastService.java
+++ cache-quickstart/src/main/java/com/kdnakt/quarkus/cache/WeatherForecastService.java
@@ -4,19 +4,27 @@ import java.time.LocalDate;
 
 import javax.enterprise.context.ApplicationScoped;
 
+import io.quarkus.cache.CacheInvalidate;
+import io.quarkus.cache.CacheKey;
 import io.quarkus.cache.CacheResult;
 
 @ApplicationScoped
 public class WeatherForecastService {
 
     @CacheResult(cacheName = "weather-cache")
-    public String getDailyForecast(LocalDate date, String city) {
+    public String getDailyForecast(@CacheKey LocalDate date, @CacheKey String city) {
         try {
             Thread.sleep(2000L);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
         return date.getDayOfWeek() + " will be " + getDailyResult(date.getDayOfMonth() % 4) + " in " + city;
+    }
+
+    @CacheInvalidate(cacheName ="weather-cache")
+    public void invalidate(@CacheKey LocalDate date, @CacheKey String city) {
+        System.out.println("invalidating ...");
+        System.out.println(city + ": " + date);
     }
 
     private String getDailyResult(int dayOfMonthModuloFour) {
