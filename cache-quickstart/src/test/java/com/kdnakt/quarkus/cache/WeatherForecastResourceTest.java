1efb119ea3fd69fb507a63128f1901078262ba78
kdnakt <a.kid.1985@gmail.com>
2020-03-22 09:17:34 +0900

    fix test

--- cache-quickstart/src/test/java/com/kdnakt/quarkus/cache/WeatherForecastResourceTest.java
+++ cache-quickstart/src/test/java/com/kdnakt/quarkus/cache/WeatherForecastResourceTest.java
@@ -4,7 +4,7 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.containsString;
 
 @QuarkusTest
 public class WeatherForecastResourceTest {
@@ -12,10 +12,11 @@ public class WeatherForecastResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/weather")
+          .when().get("/weather?city=Tokyo")
           .then()
              .statusCode(200)
-             .body(is("hello"));
+             .body(containsString("dailyForecasts"),
+                    containsString("in Tokyo"));
     }
 
 }
