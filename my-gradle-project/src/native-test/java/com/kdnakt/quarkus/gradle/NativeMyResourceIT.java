83175fd0f8a6eb3338177b33109079f6312611de
kdnakt <a.kid.1985@gmail.com>
2020-04-07 23:09:23 +0900

    recreate gradle project

---
+++ my-gradle-project/src/native-test/java/com/kdnakt/quarkus/gradle/NativeMyResourceIT.java
@@ -0,0 +1,9 @@
+package com.kdnakt.quarkus.gradle;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeMyResourceIT extends MyResourceTest {
+
+    // Execute the same tests but in native mode.
+}
