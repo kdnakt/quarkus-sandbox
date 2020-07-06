c72ce9ca9390b8b65db5306d3b3db660158ade8c
kdnakt <a.kid.1985@gmail.com>
2020-07-06 10:07:03 +0900

    add s3 project

---
+++ amazon-s3-quickstart/src/test/java/com/kdnakt/quarkus/s3/NativeS3SyncClientResourceIT.java
@@ -0,0 +1,9 @@
+package com.kdnakt.quarkus.s3;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeS3SyncClientResourceIT extends S3SyncClientResourceTest {
+
+    // Execute the same tests but in native mode.
+}
---
+++ amazon-s3-quickstart/src/test/java/com/kdnakt/quarkus/s3/S3SyncClientResourceTest.java
@@ -0,0 +1,21 @@
+package com.kdnakt.quarkus.s3;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class S3SyncClientResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/s3")
+          .then()
+             .statusCode(200)
+             .body(is("hello"));
+    }
+
+}
