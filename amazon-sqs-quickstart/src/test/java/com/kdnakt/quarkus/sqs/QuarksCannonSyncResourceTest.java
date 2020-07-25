2a6825f3d711bbe5290f4bfdecbf4c6d282ff041
kdnakt <a.kid.1985@gmail.com>
2020-07-26 00:51:27 +0900

    fix test

--- amazon-sqs-quickstart/src/test/java/com/kdnakt/quarkus/sqs/QuarksCannonSyncResourceTest.java
+++ amazon-sqs-quickstart/src/test/java/com/kdnakt/quarkus/sqs/QuarksCannonSyncResourceTest.java
@@ -1,21 +1,36 @@
 package com.kdnakt.quarkus.sqs;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import software.amazon.awssdk.http.Header;
+
+import org.hamcrest.core.IsInstanceOf;
+import org.hamcrest.text.MatchesPattern;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+
+import java.util.UUID;
+
+import com.kdnakt.quarkus.sqs.model.Quark;
 
 @QuarkusTest
 public class QuarksCannonSyncResourceTest {
 
     @Test
     public void testHelloEndpoint() {
+        Quark q = new Quark();
+        q.setFlavor("flavor");
+        q.setSpin("spin");
         given()
-          .when().get("/sync-cannon")
+          .when()
+            .contentType(ContentType.JSON)
+            .body(q)
+            .post("/sync/cannon/shoot")
           .then()
              .statusCode(200)
-             .body(is("hello"));
+             .body(MatchesPattern.matchesPattern("([a-f0-9]{8}(-[a-f0-9]{4}){4}[a-f0-9]{8})"));
     }
 
 }
