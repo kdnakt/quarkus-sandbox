8b04ed5b16a38ad306bb7cd534923a8926ce596f
kdnakt <a.kid.1985@gmail.com>
2020-08-12 09:32:42 +0900

    setup Grpc client

--- grpc-quickstart/src/main/java/com/kdnakt/quarkus/grpc/GreetingResource.java
+++ grpc-quickstart/src/main/java/com/kdnakt/quarkus/grpc/GreetingResource.java
@@ -1,16 +1,35 @@
 package com.kdnakt.quarkus.grpc;
 
+import io.quarkus.example.GreeterGrpc;
+import io.quarkus.example.HelloRequest;
+import io.quarkus.grpc.runtime.annotations.GrpcService;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/greeting")
 public class GreetingResource {
 
+    @Inject
+    @GrpcService("hello")
+    GreeterGrpc.GreeterBlockingStub client;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "hello";
     }
+
+    @GET
+    @Path("/{name}")
+    public String hello(@PathParam("name") String name) {
+        return client.sayHello(
+                HelloRequest.newBuilder().setName(name).build()
+                ).getMessage();
+    }
+
 }
