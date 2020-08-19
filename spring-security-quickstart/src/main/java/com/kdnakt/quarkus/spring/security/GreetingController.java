d6318f728f0a3b9763b8e5e8bfcee840f36e282a
kdnakt <a.kid.1985@gmail.com>
2020-08-19 22:03:00 +0900

    add spring-security-project

---
+++ spring-security-quickstart/src/main/java/com/kdnakt/quarkus/spring/security/GreetingController.java
@@ -0,0 +1,16 @@
+package com.kdnakt.quarkus.spring.security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    @GetMapping
+    public String hello() {
+        return "hello";
+    }
+}
---
+++ spring-security-quickstart/src/test/java/com/kdnakt/quarkus/spring/security/NativeGreetingControllerIT.java
@@ -0,0 +1,9 @@
+package com.kdnakt.quarkus.spring.security;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingControllerIT extends GreetingControllerTest {
+
+    // Execute the same tests but in native mode.
+}
