aaa56ae7d00653544ff9439d2fef0dfb73442d5d
kdnakt <a.kid.1985@gmail.com>
2020-08-20 08:21:08 +0900

    add Secured annotation

--- spring-security-quickstart/src/main/java/com/kdnakt/quarkus/spring/security/GreetingController.java
+++ spring-security-quickstart/src/main/java/com/kdnakt/quarkus/spring/security/GreetingController.java
@@ -1,5 +1,6 @@
 package com.kdnakt.quarkus.spring.security;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/greeting")
 public class GreetingController {
 
+    @Secured("admin")
     @GetMapping
     public String hello() {
         return "hello";
