365d71f6c538df3ac62e0fb76c1d4f993df83874
kdnakt <a.kid.1985@gmail.com>
2022-05-18 11:45:03 +0900

    readFile with vertx

--- vertx-quickstart/src/main/java/com/kdnakt/quarkus/VertxResource.java
+++ vertx-quickstart/src/main/java/com/kdnakt/quarkus/VertxResource.java
@@ -1,9 +1,13 @@
 package com.kdnakt.quarkus;
 
+import java.nio.charset.StandardCharsets;
+
 import io.vertx.mutiny.core.Vertx;
+import io.smallrye.mutiny.Uni;
 
 import javax.inject.Inject;
 import javax.ws.rs.Path;
+import javax.ws.rs.GET;
 
 @Path("/vertx")
 public class VertxResource {
@@ -13,6 +17,13 @@ public class VertxResource {
     @Inject
     public VertxResource(Vertx vertx) {
         this.vertx = vertx;
+    }
+
+    @GET
+    @Path("/lorem")
+    public Uni<String> readShortFile() {
+        return vertx.fileSystem().readFile("lorem.txt")
+                .onItem().transform(content -> content.toString(StandardCharsets.UTF_8));
     }
 
 }
