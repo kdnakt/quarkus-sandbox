d1cf006c9909db26a6d9345c2f9e283efe7ee882
kdnakt <a.kid.1985@gmail.com>
2022-05-29 17:08:42 +0900

    reactive mailer

--- mailer/src/main/java/com/kdnakt/quarkus/mailer/MailResource.java
+++ mailer/src/main/java/com/kdnakt/quarkus/mailer/MailResource.java
@@ -5,7 +5,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.GET;
 
 import io.quarkus.mailer.Mailer;
+import io.quarkus.mailer.reactive.ReactiveMailer;
 import io.quarkus.mailer.Mail;
+import io.smallrye.mutiny.Uni;
 
 @Path("/mail")
 public class MailResource {
@@ -13,12 +15,23 @@ public class MailResource {
     @Inject
     Mailer mailer;
 
+    @Inject
+    ReactiveMailer reactiveMailer;
+
     @GET
     public void sendEmail() {
         // imperative
         mailer.send(Mail.withText("mailaddress",
                 "A simple email from quarkus",
                 "This is my body."));
+    }
+
+    @GET
+    @Path("/reactive")
+    public Uni<Void> sendEmailUsingReactiveMailer() {
+        return reactiveMailer.send(Mail.withText("foo@example.com",
+                "A simple email from quarkus",
+                "This is my body using the reactive API."));
     }
 
 }
